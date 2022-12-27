6c4f4c1c564c566feb9549e5b70ecc891ca3d346
Meldin Radoncic <meldinradoncic89@gmail.com>
2022-12-27 11:52:54 -0500

    Updating all the files

--- jeopardy/jeopardy.js
+++ jeopardy/jeopardy.js
@@ -1,68 +1,29 @@
-// categories is the main data structure for the app; it looks like this:
+// // categories is the main data structure for the app; it looks like this:
 
-//  [
-//    { title: "Math",
-//      clues: [
-//        {question: "2+2", answer: 4, showing: null},
-//        {question: "1+1", answer: 2, showing: null}
-//        ...
-//      ],
-//    },
-//    { title: "Literature",
-//      clues: [
-//        {question: "Hamlet Author", answer: "Shakespeare", showing: null},
-//        {question: "Bell Jar Author", answer: "Plath", showing: null},
-//        ...
-//      ],
-//    },
-//    ...
-//  ]
-const height = 5;
-const width = 6;
+// //  [
+// //    { title: "Math",
+// //      clues: [
+// //        {question: "2+2", answer: 4, showing: null},
+// //        {question: "1+1", answer: 2, showing: null}
+// //        ...
+// //      ],
+// //    },
+// //    { title: "Literature",
+// //      clues: [
+// //        {question: "Hamlet Author", answer: "Shakespeare", showing: null},
+// //        {question: "Bell Jar Author", answer: "Plath", showing: null},
+// //        ...
+// //      ],
+// //    },
+// //    ...
+// //  
+
+
+const NUM_CATEGORIES = 6;
+const NUM_QUESTIONS_PER_CAT = 5;
+const JEOPARDY_API_URL = 'https://jservice.io/api/';
+
 let categories = [];
-
- // function structureTable() {
- //   for(let i=0; i<height; i++) {
- //     categories.push(Array.from({length:width}));
- //   }
- //   console.log(categories);
- // }
-
- function makeTable() {
-   const table = document.createElement('table');
-   table.classList.add('table')
-   const thead = document.createElement('thead');
-   const tr = document.createElement('tr')
-
-   for(let i=0; i<width; i++) {
-     const th = document.createElement('th');
-     th.innerText = categories[i].title;
-     th.classList.add('column-th')
-     tr.append(th)
-   }
-
-   thead.append(tr);
-   table.append(thead);
-
-   const tbody = document.createElement('tbody');
-
-   for(let y=0; y<height; y++) {
-       const row  = document.createElement('tr');
-       for(let x=0; x<width; x++) {
-         const cell= document.createElement('td')
-         cell.setAttribute('id',`${y}-${x}`);
-         cell.classList.add('cell')
-         cell.innerText= '$'
-         row.append(cell);
-       }
-        tbody.append(row)
-     }
-
-
-table.append(tbody)
-   document.body.prepend(table);
-
- }
 
 /** Get NUM_CATEGORIES random category from API.
  *
@@ -70,17 +31,13 @@ table.append(tbody)
  */
 
 async function getCategoryIds() {
-  const radndomCategoriesId=[];
-  for(let i = 0; i <= 10; i++) {
-  let response = await axios.get('https://jservice.io/api/random');
-  radndomCategoriesId.push(response.data[0].category.id);
+  const response = await axios.get(`${JEOPARDY_API_URL}categories`, {
+    params: {
+      count: NUM_CATEGORIES
+    }
+  });
+  return response.data.map(category => category.id);
 }
-let shuffled_array = _.shuffle(radndomCategoriesId);
-shuffled_array.splice(width,shuffled_array.length);
-    return shuffled_array;
-}
-
-
 
 /** Return object with data about a category:
  *
@@ -94,22 +51,22 @@ shuffled_array.splice(width,shuffled_array.length);
  *   ]
  */
 
- async function getCategory(catId) {
-   let response = await axios.get(`https://jservice.io/api/category?id=${catId}`); //Id 300 problem
-
-  const clues = getNRandomClues(response.data.clues)
-  let categoryInfo = {
-    title:response.data.title,
-    clues,
-  }
-  return categoryInfo;
-}
-
-function getNRandomClues(clues, n=height) {
-  const shuffled_array = _.shuffle(clues);
-  shuffled_array.splice(n,shuffled_array.length);
-
-  return shuffled_array;
+async function getCategory(catId) {
+  const response = await axios.get(`${JEOPARDY_API_URL}category`, {
+    params: {
+      id: catId
+    }
+  });
+  return {
+    title: response.data.title,
+    clues: _.shuffle(response.data.clues).slice(0, NUM_QUESTIONS_PER_CAT).map(clue => {
+      return {
+        question: clue.question,
+        answer: clue.answer,
+        showing: null
+      };
+    })
+  };
 }
 
 /** Fill the HTML table#jeopardy with the categories & cells for questions.
@@ -120,59 +77,98 @@ function getNRandomClues(clues, n=height) {
  *   (initally, just show a "?" where the question/answer would go.)
  */
 
-  async function fillTable() {
+async function fillTable() {
+  const thead = $('#jeopardy thead');
+  const tbody = $('#jeopardy tbody');
 
+  // clear existing table
+  thead.empty();
+  tbody.empty();
+
+  // create header row
+  const tr = $('<tr>');
+  for (let i = 0; i < categories.length; i++) {
+    const td = $('<td>').text(categories[i].title);
+    tr.append(td);
+  }
+  thead.append(tr);
+
+  // create question rows
+  for (let i = 0; i < NUM_QUESTIONS_PER_CAT; i++) {
+    const tr = $('<tr>');
+    for (let j = 0; j < categories.length; j++) {
+      const td = $('<td>').text('?').addClass('question').data('clue', categories[j].clues[i]);
+      tr.append(td);
+      tbody.append(tr);
+    }
+  }
+  
+  /** Handle clicking on a clue: show the question or answer.
+   *
+   * Uses .showing property on clue to determine what to show:
+   * - if currently null, show question & set .showing to "question"
+   * - if currently "question", show answer & set .showing to "answer"
+   * - if currently "answer", ignore click
+   * */
+  
+  function handleClick(evt) {
+    const clue = $(evt.target).data('clue');
+    if (clue.showing === null) {
+      $(evt.target).text(clue.question).removeClass('question').addClass('question-shown');
+      clue.showing = 'question';
+    } else if (clue.showing === 'question') {
+      $(evt.target).text(clue.answer).removeClass('question-shown').addClass('answer-shown');
+      clue.showing = 'answer';
+    }
+  }
+  
+  /** Wipe the current Jeopardy board, show the loading spinner,
+   * and update the button used to fetch data.
+   */
+  
+  function showLoadingView() {
+    $('#jeopardy').hide();
+    $('#spin-container').show();
+    $('#start').text('Restart').attr('disabled', true);
+  }
+  
+  /** Remove the loading spinner and update the button used to fetch data. */
+  
+  function hideLoadingView() {
+    $('#jeopardy').show();
+    $('#spin-container').hide();
+    $('#start').text('Start').attr('disabled', false);
+  }
+  
+  /** Start game:
+   *
+   * - get random category Ids
+   * - get data for each category
+   * - create HTML table
+   * */
+  
+  async function setupAndStart() {
+    showLoadingView();
+    categories = [];
+    const categoryIds = await getCategoryIds();
+    for (let i = 0; i < categoryIds.length; i++) {
+      const category = await getCategory(categoryIds[i]);
+      categories.push(category);
+    }
+    fillTable();
+    hideLoadingView();
+  }
+  
+
+ /** On click of start / restart button, set up game. */
+ $('#start').click(setupAndStart);
+  
+ /** On page load, add event handler for clicking clues */
+ 
+ $('#jeopardy tbody').on('click', '.question, .question-shown', function(evt) {
+    handleClick(evt);
+  });
 }
 
 
-/** Handle clicking on a clue: show the question or answer.
- *
- * Uses .showing property on clue to determine what to show:
- * - if currently null, show question & set .showing to "question"
- * - if currently "question", show answer & set .showing to "answer"
- * - if currently "answer", ignore click
- * */
-
-function handleClick(evt) {
-}
-
-/** Wipe the current Jeopardy board, show the loading spinner,
- * and update the button used to fetch data.
- */
-
-function showLoadingView() {
-
-}
-
-/** Remove the loading spinner and update the button used to fetch data. */
-
-function hideLoadingView() {
-}
-
-/** Start game:
- *
- * - get random category Ids
- * - get data for each category
- * - create HTML table
- * */
-
-async function setupAndStart() {
-  const catIds = await getCategoryIds();
-
-  catIds.forEach(async id => {
-    const cat = await getCategory(id);
-    categories.push(cat);
-  })
-
-  // makeTable()
-}
-
-setupAndStart()
-
-/** On click of start / restart button, set up game. */
-
-// TODO
-
-/** On page load, add event handler for clicking clues */
-
-// TODO
+fillTable()
